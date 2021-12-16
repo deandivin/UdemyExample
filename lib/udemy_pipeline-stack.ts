38a0c0c38edaf34f74cd028b134068fdc8f948a5
deandivin <ddivincenzo@promet.ai>
2021-12-16 14:20:43 -0500

    commit

--- lib/udemy_pipeline-stack.ts
+++ lib/udemy_pipeline-stack.ts
@@ -1,15 +1,56 @@
 import * as cdk from '@aws-cdk/core';
+import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline';
+import { SecretValue } from '@aws-cdk/core';
+import {CodeBuildAction, GitHubSourceAction} from '@aws-cdk/aws-codepipeline-actions'
+import { BuildSpec, LinuxBuildImage, PipelineProject } from '@aws-cdk/aws-codebuild';
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class UdemyPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new Pipeline(this, 'Pipeline', {
+      pipelineName: 'UdemyPipeline',
+      crossAccountKeys: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'UdemyPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const sourceOutput = new Artifact('SourceOutput');
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions:[
+        new GitHubSourceAction({
+          owner: 'deandivin',
+          repo:'UdemyExample',
+          branch: 'main',
+          actionName: 'PipelineSource',
+          oauthToken: SecretValue.secretsManager('udemy-pipeline-token'),
+          output: sourceOutput
+        }
+
+        )
+
+      ]
+    });
+
+    const cdkBuildOutput = new Artifact('CdkBuildOutput');
+
+    pipeline.addStage({
+      stageName: 'Codebuild',
+      actions: [new CodeBuildAction({
+          actionName: 'CDK_Build',
+          input :sourceOutput,
+          output :cdkBuildOutput,
+          project: new PipelineProject(this, `CDKBuildProject`, {
+            environment:{
+              buildImage: LinuxBuildImage.STANDARD_5_0
+            },
+            buildSpec: BuildSpec.fromSourceFilename(
+              "build-specs/cdk-build-spec.yml"
+            ),
+        }),
+      }),
+      ],
+    });
   }
 }
